4cee919313064ef420905f5b567fe7276f296de7
Matthew Brett <matthew.brett@gmail.com>
2016-06-24 15:32:58 -0700

    Go the long way round for 32-bit build on OSX

--- config.sh
+++ config.sh
@@ -1,26 +1,33 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-function pre_build {
-    source multibuild/library_builders.sh
-    build_hdf5
-    if [ -n "$IS_OSX" ]; then
-        return
+function build_wheel {
+    local repo_dir=${1:-$REPO_DIR}
+    if [ -z "$IS_OSX" ]; then
+        build_linux_wheel $@
+    else
+        build_osx_wheel $@
     fi
+}
+
+function build_libs {
+    build_hdf5
+}
+
+function build_linux_wheel {
+    source multibuild/library_builders.sh
+    build_libs
     # Add workaround for auditwheel bug:
     # https://github.com/pypa/auditwheel/issues/29
     local bad_lib="/usr/local/lib/libhdf5.so"
     if [ -z "$(readelf --dynamic $bad_lib | grep RUNPATH)" ]; then
         patchelf --set-rpath $(dirname $bad_lib) $bad_lib
     fi
+    build_pip_wheel $@
 }
 
-function build_wheel {
+function build_osx_wheel {
     local repo_dir=${1:-$REPO_DIR}
-    if [ -z "$IS_OSX" ]; then
-        build_pip_wheel "$repo_dir"
-        return
-    fi
     local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
     # Build dual arch wheel
     export CC=clang
@@ -29,15 +36,29 @@ function build_wheel {
     # 32-bit wheel
     export CFLAGS="-arch i386"
     export FFLAGS="-arch i386"
+    export LDFLAGS="-arch i386"
+    # Build libraries
+    source multibuild/library_builders.sh
+    build_libs
+    # Build wheel
+    local py_ld_flags="-Wall -undefined dynamic_lookup -bundle"
     local wheelhouse32=${wheelhouse}32
     mkdir -p $wheelhouse32
+    export LDFLAGS="$LDFLAGS $py_ld_flags"
+    export LDSHARED="clang $LDFLAGS $py_ld_flags"
     build_pip_wheel "$repo_dir"
     mv ${wheelhouse}/*whl $wheelhouse32
     # 64-bit wheel
     export CFLAGS="-arch x86_64"
     export FFLAGS="-arch x86_64"
+    export LDFLAGS="-arch x86_64"
+    unset LDSHARED
     # Force rebuild of all libs
     rm *-stamp
+    build_libs
+    # Build wheel
+    export LDFLAGS="$LDFLAGS $py_ld_flags"
+    export LDSHARED="clang $LDFLAGS $py_ld_flags"
     build_pip_wheel "$repo_dir"
     # Fuse into dual arch wheel(s)
     for whl in ${wheelhouse}/*.whl; do
