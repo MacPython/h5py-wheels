435c8578f6b58e8c657adb5c8573df8ec4d3bf51
Matthew Brett <matthew.brett@gmail.com>
2016-07-06 15:32:14 +0100

    Add zlib encoding for Linux, test filters
    Make sure zlib encoding installed for hdf5 library.
    
    Check gzip encoding present in built h5py.
    
    Closes gh-731.

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,19 @@
-# Run h5py tests
+# Run h5py tests, check for expected filters
+from __future__ import print_function
 import sys
 import h5py
-res = h5py.run_tests(verbose=3)
-sys.exit(not res.wasSuccessful())
+
+default_filters = set(('shuffle', 'gzip', 'fletcher32', 'scaleoffset'))
+szip_filters = default_filters.union(('szip',))
+
+good = bool(h5py.run_tests(verbose=3))
+
+decode_missing = szip_filters.difference(h5py.filters.decode)
+if decode_missing:
+    print('Missing decode filters', ' '.join(decode_missing))
+    good = False
+encode_missing = default_filters.difference(h5py.filters.encode)
+if encode_missing:
+    print('Missing encode filters', ' '.join(encode_missing))
+    good = False
+sys.exit(not good)
